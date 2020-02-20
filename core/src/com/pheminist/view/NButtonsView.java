46c69e6e0010edebcf35aa7f6ed34285a36ce5ca
Pheminist <phild@bk.ru>
2020-02-20 11:09:03 +0300

    shift + - changes tones in Beeper and NoteButton labels.

--- core/src/com/pheminist/view/NButtonsView.java
+++ core/src/com/pheminist/view/NButtonsView.java
@@ -7,41 +7,37 @@ import com.pheminist.interfaces.IListener;
 import com.pheminist.model.MIDI.HData;
 import com.pheminist.model.Model;
 import com.pheminist.model.NoteEvent;
+import com.pheminist.model.Shift;
 
 public class NButtonsView extends Table implements IListener<NoteEvent> {
+    Model model;
     HData hData;
-//    Hren parent;
 
     public NButtonsView(Model model) {
+        this.model=model;
         hData = model.gethData();
-//        this.parent = parent;
-//        Skin skin = parent.assets.get(Hren.SKIN, Skin.class);
 
         for (int i=0;i<hData.getnTones();i++){
-//            TextButton noteButton=new TextButton(HData.octaveAndNoteName(hData.getTones()[i]),skin);
-//            noteButton.getLabel().setFontScale(noteButton.getLabel().getScaleX());
             NoteButton noteButton=new NoteButton(model,i);
             this.add(noteButton).expandX().fillX().uniform().minWidth(10);
         }
 
         model.noteEvent.getPublisher().addListener(this);
 
-    }
+        model.shift.getPublisher().addListener(new IListener<Shift>() {
+            @Override
+            public void on(Shift event) {
+                setNButtonsLabels();
+            }
+        });
 
-//    @Override
-//    public void noteEvent(int note, int tone, boolean isOn) {
-//        ((NoteButton)(getChildren().get(note))).setNote(isOn);
-//
-//    }
+    }
 
     public void setNButtonsLabels(){
         SnapshotArray<Actor> noteButtons=getChildren();
         for(int i=0;i<noteButtons.size;i++){
-//            ((NoteButton)noteButtons.get(i)).setToneLabel(hData.getTones()[i]+parent.getPreferences().getShift());
-            ((NoteButton)noteButtons.get(i)).setToneLabel(hData.getTones()[i]);
+            ((NoteButton)noteButtons.get(i)).setToneLabel(hData.getTones()[i]+model.shift.getShift());
         }
-
-
     }
 
     @Override
