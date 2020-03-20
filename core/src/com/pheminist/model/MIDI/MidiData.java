69ca81e4df3d1a029a033a5c9c606f4dbce7b9c7
Pheminist <phild@bk.ru>
2020-03-20 13:54:44 +0300

    MReader

--- core/src/com/pheminist/model/MIDI/MidiData.java
+++ core/src/com/pheminist/model/MIDI/MidiData.java
@@ -17,17 +17,15 @@ public class MidiData {
     public Track[] toTracks(byte[] bytes) {
         MReader mReader = new MReader(bytes);
         Track[] tracks = new Track[midiHeader.getNumOfTracks()];
+        mReader.setIndex(HEADER_LENGTH);
 
         for (int i = 0; i < midiHeader.getNumOfTracks(); i++) {  // fill the array of tracks
-            mReader.setIndex(HEADER_LENGTH);
-
             mReader.readString(4); // Копируем имя раздела.
             int length = (int) mReader.read32BigEndian(); // 4 байта длинны всего блока.
             int startIndex = mReader.getIndex();
             int endIndex = startIndex + length;
             Track track = new Track(Arrays.copyOfRange(bytes, startIndex, endIndex));
             mReader.setIndex(endIndex);
-
             tracks[i] = track;
         }
         return tracks;
