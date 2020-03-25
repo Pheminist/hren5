794a050f7338bcfaba07c31789f35f055dde29eb
Pheminist <phild@bk.ru>
2020-03-25 11:13:00 +0300

    glEsVersion="0x00020000 in manifest

--- core/src/com/pheminist/model/Beeper.java
+++ core/src/com/pheminist/model/Beeper.java
@@ -13,40 +13,40 @@ public class Beeper implements IListener<NoteEvent> {
 
     Beeper(Model model) {
 
-        model.shift.getPublisher().addListener(new IListener<Shift>() {
-            @Override
-            public void on(Shift event) {
-                Beeper.this.setShift(event.getShift());
-            }
-        });
-
-        try {
-            synth = MidiSystem.getSynthesizer();
-            synth.open();
-            channels = synth.getChannels();
-            channels[0].programChange(22);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        model.shift.getPublisher().addListener(new IListener<Shift>() {
+//            @Override
+//            public void on(Shift event) {
+//                Beeper.this.setShift(event.getShift());
+//            }
+//        });
+//
+//        try {
+//            synth = MidiSystem.getSynthesizer();
+//            synth.open();
+//            channels = synth.getChannels();
+//            channels[0].programChange(22);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 
     public void allNotesOff() {
-        channels[0].allNotesOff();
+//        channels[0].allNotesOff();
     }
 
     private void setShift(int shift) {
-        allNotesOff();
-        this.shift = shift;
+//        allNotesOff();
+//        this.shift = shift;
     }
 
     @Override
     public void on(NoteEvent event) {
-        int tone = event.getTone() + shift;
-        if (event.isOn()) channels[0].noteOn(tone, 80);
-        else channels[0].noteOff(tone);
+//        int tone = event.getTone() + shift;
+//        if (event.isOn()) channels[0].noteOn(tone, 80);
+//        else channels[0].noteOff(tone);
     }
 
     void dispose() {
-        synth.close();
+//        synth.close();
     }
 }
